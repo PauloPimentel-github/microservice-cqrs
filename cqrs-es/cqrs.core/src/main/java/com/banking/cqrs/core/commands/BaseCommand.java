9e2dd7a9b554ee13d88534c76bd9f219db349215
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-04-08 22:04:50 -0300

    Adicionando criação de Commands

---
+++ cqrs-es/cqrs.core/src/main/java/com/banking/cqrs/core/commands/BaseCommand.java
@@ -0,0 +1,15 @@
+package com.banking.cqrs.core.commands;
+
+import com.banking.cqrs.core.messages.Message;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public abstract class BaseCommand extends Message {
+
+    protected BaseCommand(String id){
+        super(id);
+    }
+
+}
