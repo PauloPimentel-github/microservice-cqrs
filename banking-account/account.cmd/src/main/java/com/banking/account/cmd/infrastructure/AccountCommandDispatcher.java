1a780e2f35d45787b1f9359ad3d056df2f48bd83
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-04-11 20:27:19 -0300

    Adicionando implementação de Commands e Events em Mediator

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountCommandDispatcher.java
@@ -0,0 +1,34 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.cqrs.core.commands.BaseCommand;
+import com.banking.cqrs.core.commands.CommandHandlerMethod;
+import com.banking.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class AccountCommandDispatcher implements CommandDispatcher {
+
+    private final Map<Class<? extends BaseCommand>, List<CommandHandlerMethod>> routes = new HashMap<>();
+
+    @Override
+    public <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler) {
+        var handlers = routes.computeIfAbsent(type, c -> new LinkedList<>());
+        handlers.add(handler);
+    }
+
+    @Override
+    public void send(BaseCommand command) {
+        var handlers = routes.get(command.getClass());
+        if(Objects.isNull(handlers) || handlers.isEmpty()) {
+            throw new RuntimeException("The command handler was not registered.");
+        }
+
+        if(handlers.size() > 1){
+            throw new RuntimeException("Cannot send a command that has more than one handler.");
+        }
+
+        handlers.get(0).handle(command);
+    }
+}
