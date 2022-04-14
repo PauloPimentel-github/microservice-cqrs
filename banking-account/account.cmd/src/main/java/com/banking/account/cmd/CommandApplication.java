4bc4cd226c7ca29f47adf409280d5802c0a05e49
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-04-13 22:48:53 -0300

    Registrando Command Handler

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/CommandApplication.java
@@ -0,0 +1,32 @@
+package com.banking.account.cmd;
+
+import com.banking.account.cmd.api.command.*;
+import com.banking.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class CommandApplication {
+
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+
+	@Autowired
+	private CommandHandler commandHandler;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CommandApplication.class, args);
+	}
+
+	@PostConstruct
+	public void registerHandlers(){
+		this.commandDispatcher.registerHandler(OpenAccountCommand.class, this.commandHandler::handle);
+		this.commandDispatcher.registerHandler(DepositFundsCommand.class,  this.commandHandler::handle);
+		this.commandDispatcher.registerHandler(WithdrawFundsCommand.class,  this.commandHandler::handle);
+		this.commandDispatcher.registerHandler(CloseAccountCommand.class,  this.commandHandler::handle);
+	}
+}
